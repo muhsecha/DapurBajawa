8c717e44c16786e5f1a1468f8886ffebf02964c8
Nelvari <nelvari2018rplrus2@gmail.com>
2020-11-15 09:52:55 +0700

    login dan view data java

---
+++ app/src/main/java/com/example/dapurbajawa/BaseUrl.java
@@ -0,0 +1,5 @@
+package com.example.dapurbajawa;
+
+public class BaseUrl {
+    public static String url = "http://192.168.1.4/apibajawa/";
+}
--- app/src/main/java/com/example/dapurbajawa/ViewAdapter.java
+++ app/src/main/java/com/example/dapurbajawa/ViewAdapter.java
@@ -1,10 +1,13 @@
 package com.example.dapurbajawa;
 
+import android.content.Intent;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -21,12 +24,29 @@ public class ViewAdapter extends RecyclerView.Adapter<ViewAdapter.ViewViewHolder
     @NonNull
     @Override
     public ViewViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        viewku = layoutInflater.inflate(R.layout.list_item, parent, false);
+        return new ViewViewHolder(viewku);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull final ViewViewHolder holder, final int position) {
+        holder.tvNama.setText(dataList.get(position).getNama());
+        holder.tvTlp.setText(dataList.get(position).getNohp());
+        holder.tvWaktu.setText(dataList.get(position).getTanggal());
+        holder.cvInbox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(holder.itemView.getContext(), DetailDataActivity.class);
+                intent.putExtra("id", dataList.get(position).getId());
+                intent.putExtra("nama", dataList.get(position).getNama());
+                intent.putExtra("tanggal", dataList.get(position).getTanggal());
+                intent.putExtra("nohp", dataList.get(position).getNohp());
+                intent.putExtra("note", dataList.get(position).getNote());
+                intent.putExtra("kode", dataList.get(position).getKode());
+                intent.putExtra("status", dataList.get(position).getStatus());
+            }
+        });
     }
 
     @Override
@@ -35,10 +55,15 @@ public class ViewAdapter extends RecyclerView.Adapter<ViewAdapter.ViewViewHolder
     }
 
     public class ViewViewHolder extends RecyclerView.ViewHolder {
-        public ViewViewHolder(@NonNull View itemView) {
-            super(itemView);
-            TextView txtnama,txtnotelp;
+        TextView tvNama, tvTlp, tvWaktu;
+        CardView cvInbox;
 
+        ViewViewHolder(View itemView) {
+            super(itemView);
+            tvNama =itemView.findViewById(R.id.tvNama);
+            tvTlp =itemView.findViewById(R.id.tvnotlp);
+            tvWaktu =itemView.findViewById(R.id.tvwaktu);
+            cvInbox =itemView.findViewById(R.id.cdPesan);
 
         }
     }
