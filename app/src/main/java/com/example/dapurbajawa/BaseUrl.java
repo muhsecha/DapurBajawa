8550fca7550f7e797bff466149610f614b29ce62
Hasan Wijaya <hasanwijaya.xrpl2018smkrus@gmail.com>
2021-08-02 17:05:45 +0700

    update base url

--- app/src/main/java/com/example/dapurbajawa/BaseUrl.java
+++ app/src/main/java/com/example/dapurbajawa/BaseUrl.java
@@ -1,5 +1,5 @@
 package com.example.dapurbajawa;
 
 public class BaseUrl {
-    public static String url = "http://192.168.6.188/apibajawa/";
+    public static String url = "https://supporting-compiler.000webhostapp.com/";
 }
